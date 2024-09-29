fba1fcbefdbdfd2251db0cc7077ca8ae8efc6464
V <username@example.com>
2024-09-30 01:55:01 +0500

    New Update

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
 const manifestUrl =
-  'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
+  'https://veebull.github.io/first_contract_front_end/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
